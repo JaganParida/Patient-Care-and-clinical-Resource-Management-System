eaf89db3b44817659f14e0bb7ed163edcc6cbb03
Swayamjit Biswal <swoyamjitbiswal@gmail.com>
2025-10-14 11:21:55 +0530

    Update in appointment in user servlet

--- src/main/java/com/user/servlet/Appointment.java
+++ src/main/java/com/user/servlet/Appointment.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 @WebServlet("/appAppointment")
 public class AppointmentServlet extends HttpServlet {
 
